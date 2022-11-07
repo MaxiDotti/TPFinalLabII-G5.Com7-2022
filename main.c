c7b8d623ebc47eabcd36459b9611b06ea515268a
Mateo <mateo.mdq123@gmail.com>
2022-11-07 19:10:03 -0300

    Estructuras

--- main.c
+++ main.c
@@ -9,31 +9,29 @@ typedef struct{
     float sueldo;
     int valorpase;
     char posicion[20]; /// ARQUERO - DEF - MEDIOC - DELANTERO
-    int categoria; /// 1RA / 3RA / 4TA
     int goles;
     int activo; /// 1 ACTIVO / 0 INACTIVO
-}stJugador; /// REGISTRO ARCHIVO
+}stJugador;
 
-typedef struct{
+typedef struct ArbolJugador{
     stJugador dato;
     struct nodoJugador *der;
     struct nodoJugador *izq;
 }ArbolJugador;
 
 typedef struct{
-    int nrosocio;
+    int idCT;
     char nombre[20];
     char apellido[20];
     int dni;
     int tel;
-    char categoria[20]; /// SIMPLE - PLENO
-}stSocio;
+}stCT;///CUERPO TECNICO
 
-typedef struct{
-    stSocio dato;
-    struct nodoJugador *ant;
-    struct nodoJugador *sig;
-}nodoSocio;
+typedef struct nodoCT{
+    stCT dato;
+    struct nodoCT *ant;
+    struct nodoCT *sig;
+}nodoCT;
 
 typedef struct{
     int legajo;
@@ -41,26 +39,25 @@ typedef struct{
     char apellido[20];
     int dni;
     int tel;
-    char puesto[20]; /// DT - AYUDANTE - PROFE - MEDICO - MAESTRANZA
+    char puesto[20]; /// AYUDANTE - PROFE - MEDICO - MAESTRANZA
     float sueldo;
 }stEmpleado;
 
-typedef struct{
-    stSocio dato;
-    struct nodoJugador *sig;
+typedef struct nodoEmpleado{
+    stEmpleado dato;
+    struct nodoEmpleado *sig;
 }nodoEmpleado;
 
-/*
-typedef struct{
-    nodoJugador *jugadores; /// pos 0
-    nodoEmpleado *empleados; /// pos 1
-    nodoSocio *socios; /// pos 2
-}Club;
-*/
 
+typedef struct celdaDivision{
+    char nombreDivision[15]; /// primera , segunda , tercera
+    int idDivision; /// 1, 2 ,3
+    nodoEmpleado* emp;
+    nodoCT* ct;
+    ArbolJugador* jug;
+}celdaDivision;
 
 int main()
 {
-    printf("tp!\n");
     return 0;
 }
