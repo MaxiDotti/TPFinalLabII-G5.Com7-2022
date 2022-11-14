88548b000be2437374cfc27f60040bedee265dc4
MaxiDotti <dottimaximo@hotmail.com>
2022-11-14 12:54:51 -0300

    Modif 14.11 13:00 hs

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include "empleado.h"
 int main()
 {
-<<<<<<< HEAD
     char archivoJugador[] = "ArchivoJugadores";
     ///cargarJugadorRegistroArchivo(archivoJugador);
     ///mostrarArchivoJugador(archivoJugador);
@@ -12,28 +11,10 @@ int main()
     arbolJugador *arbol = inicArbol();
     arbol = archivoToArbol(archivoJugador);
     mostrarArbolDNI(arbol);
-    arbolJugador *goleador = inicArbol();
-    arbolJugador *goleadores = inicArbol();
-    int i=0; /// SE INICIALIZA EL CONTADOR EN 0
-    goleadores = top3Goleadores(arbol, goleador, goleadores, i);
-    printf("\n\n------------------> GOLEADORES\n\n");
-    if(goleadores){
-        mostrarArbolDNI(goleadores);
-    }
-    else{
-        printf("\nSALIO COMO EL ORTO\n");
-    }
-/*
-    printf("\n\nARBOL ORDENADO POR D.N.I.\n\n");
-    mostrarArbolDNI(arbol);
-*/
-=======
 
 
 
 
-
->>>>>>> 71b06a2d261b73959dfb0d86db2c37a51e071a0f
     return 0;
 }
 
